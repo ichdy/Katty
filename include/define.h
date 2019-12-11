b9bca71604863fe12ce69f4d2f94ae13bddd25b3
Ichdyan Thalasa <ichdyan.thalasa@gmail.com>
2019-12-11 13:05:22 +0700

    add chatstatus message type

--- include/define.h
+++ include/define.h
@@ -11,6 +11,7 @@ enum MessageType {
     MessageDataList,
     MessageStatus,
     MessagePrivateChat,
+    MessagePrivateChatStatus,
     MessagePrivateSendFile,
     MessagePrivateHistory,
     MessagePrivatePending,
